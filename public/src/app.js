0266ab453bb9b758f98eb787debcc220b20907db
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-15 17:13:36 -0700

    Implement basic HelloUser component with state.

--- public/src/app.js
+++ public/src/app.js
@@ -8,4 +8,25 @@ var HelloWorld = React.createClass({
   }
 });
 
-React.render(<HelloWorld />, document.getElementById('app'));
+var HelloUser = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '@kalevrk'
+    }
+  },
+  handleChange: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        Hello {this.state.username} <br />
+        Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+      </div>
+    )
+  }
+});
+
+React.render(<HelloUser />, document.getElementById('app'));
