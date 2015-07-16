f649fff01d88b98a3b484fbd83a394e672ae6891
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-15 17:29:31 -0700

    Create set of nested React components with state and props.

--- public/src/app.js
+++ public/src/app.js
@@ -29,4 +29,37 @@ var HelloUser = React.createClass({
   }
 });
 
-React.render(<HelloUser />, document.getElementById('app'));
+var FriendsContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      name: 'Kalev Roomann-Kurrik',
+      friends: ['Arne Roomann-Kurrik', 'Katrina Uychaco', 'Mauno Kork']
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <h3> Name: {this.state.name} </h3>
+        <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function() {
+    var listItems = this.props.names.map(function(friend) {
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+React.render(<FriendsContainer />, document.getElementById('app'));
